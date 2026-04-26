845a13ae9692c490c1f22e5aeb39b18e141e3736
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-04-26 14:57:29 +0100

    feat: improve enemy patrol movement, add logic to damage player upon enemy collision

--- Assets/_2DAdventureGame/Scripts/EnemyController.cs
+++ Assets/_2DAdventureGame/Scripts/EnemyController.cs
@@ -5,23 +5,50 @@ public class EnemyController : MonoBehaviour
     private Rigidbody2D rb;
     public float speed = 1.0f;
 
+    public bool vertical;
+
+    public float changeTime = 3.0f;
+    float timer;
+    int direction = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        timer = changeTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 position = rb.position;
-        position.x += speed * Time.deltaTime;
+
+        if (vertical)
+        {
+            position.y += speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            position.x += speed * direction * Time.deltaTime;
+        }
 
         rb.MovePosition(position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller == null) return;
+
+        controller.ChangeHealth(-1);
     }
 }
