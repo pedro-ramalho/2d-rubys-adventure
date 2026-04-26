28ffef11b8a7048f995cbf03b6d51dd6512061af
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-04-26 14:43:58 +0100

    feat: set up a damage zone that damages the player over time

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -10,23 +10,37 @@ public class PlayerController : MonoBehaviour
     // Player health
     public int maxHealth = 5;
     public int health { get { return currentHealth; }}
-    int currentHealth = 4;
+    int currentHealth;
 
     // Player speed
     public float speed = 3.0f;
+
+    // Invicibility
+    public float timeInvicible = 2.0f;
+    bool isInvicible;
+    float damageCooldown;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction.Enable();
         rb = GetComponent<Rigidbody2D>();
-        //currentHealth = maxHealth;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = moveAction.ReadValue<Vector2>();
+
+        if (isInvicible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvicible = false;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -37,6 +51,14 @@ public class PlayerController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvicible) return;
+
+            isInvicible = true;
+            damageCooldown = timeInvicible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
