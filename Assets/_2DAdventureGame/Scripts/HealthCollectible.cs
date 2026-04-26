75fad4c8f38badfe0733e6658e22f6ba7afe0a5d
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-04-26 14:29:24 +0100

    feat: implement simple health system, create health collectible

---
+++ Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller == null) return;
+
+        if (controller.health < controller.maxHealth)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -4,18 +4,40 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction moveAction;
+    private Rigidbody2D rb;
+    private Vector2 move;
+
+    // Player health
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
+    int currentHealth = 4;
+
+    // Player speed
+    public float speed = 3.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction.Enable();
+        rb = GetComponent<Rigidbody2D>();
+        //currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = moveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+        move = moveAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb.position + move * speed * Time.deltaTime;
+        rb.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
