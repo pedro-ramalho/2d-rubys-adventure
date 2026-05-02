2d8655b4ec0817732dbadcaccce042f38ca00341
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-02 10:16:47 +0100

    refactor: clean up projectile script

--- Assets/_2DAdventureGame/Scripts/Projectiles/Projectile.cs
+++ Assets/_2DAdventureGame/Scripts/Projectiles/Projectile.cs
@@ -1,36 +1,30 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rb;
+    [SerializeField] private float maxLifetime = 3f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.magnitude > 100.0f)
-        {
-            Destroy(gameObject);
-        }
-    }
-
     public void Launch(Vector2 direction, float force)
     {
         rb.AddForce(direction * force);
+        Destroy(gameObject, maxLifetime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        EnemyController enemy = other.GetComponent<EnemyController>();
-        if (enemy != null) enemy.Fix();
+        if (other.TryGetComponent(out EnemyController enemy)) enemy.Fix();
 
         Destroy(gameObject);
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
