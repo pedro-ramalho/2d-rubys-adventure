18ce5139be64a8be9d7158abd74571edd5c989c9
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-05 19:28:51 +0100

    feat: created an abstract state class for modeling generic states

---
+++ Assets/_2DAdventureGame/Scripts/State/State.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary>
+/// Abstract class for modeling a generic state. 
+/// </summary>
+/// <typeparam name="T">The type of the owner of the state.</typeparam>
+public abstract class State<T>
+{
+    /// <summary>
+    /// Method that is executed when a transition happens <b>to</b> this state.
+    /// It should be used for setting up the necessary context for the state to operate as intended.
+    /// </summary>
+    /// <param name="owner">A reference to the owner of the state's context.</param>
+    public abstract void Enter(T owner);
+
+    /// <summary>
+    /// Method that is executed on every frame whilst the state is active.
+    /// </summary>
+    /// <param name="owner">A reference to the owner of the state's context.</param>
+    public abstract void Update(T owner);
+
+    /// <summary>
+    /// Method that is executed when a transition happens <b>from</b> this state.
+    /// It should be used for clearing up the context for the state machine to remain operating normally.
+    /// </summary>
+    /// <param name="owner">A reference to the owner of the state's context.</param>
+    public abstract void Exit(T owner); 
+}
