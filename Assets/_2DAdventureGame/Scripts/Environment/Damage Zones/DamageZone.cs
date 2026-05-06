317875400f4cb6fe9171b8a04d76b60a3f1c54b3
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-06 20:47:49 +0100

    feat: fix issue in DamageZone.cs where it was healing the Player instead of damaging them

--- Assets/_2DAdventureGame/Scripts/Environment/Damage Zones/DamageZone.cs
+++ Assets/_2DAdventureGame/Scripts/Environment/Damage Zones/DamageZone.cs
@@ -7,6 +7,6 @@ public class DamageZone : MonoBehaviour
         Player player = other.GetComponent<Player>();
         if (player == null) return;
 
-        player.ChangeHealth(1);
+        player.ChangeHealth(-1);
     }
 }
