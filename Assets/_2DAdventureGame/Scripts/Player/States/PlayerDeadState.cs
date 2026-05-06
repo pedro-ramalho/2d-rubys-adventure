30e56c6240a0bf133d9174db5e0a693cf881d8d9
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-06 12:41:34 +0100

    feat: implement PlayerDeadState

--- Assets/_2DAdventureGame/Scripts/Player/States/PlayerDeadState.cs
+++ Assets/_2DAdventureGame/Scripts/Player/States/PlayerDeadState.cs
@@ -2,5 +2,41 @@ using UnityEngine;
 
 public class PlayerDeadState : PlayerState
 {
-    
+    /// <summary>
+    /// <c>Enter</c> shuts everything down.
+    /// This includes clearing the player's current velocity to 0 (preventing them from walking),
+    /// stopping the audio source, and resetting the animator back to an idle pose.
+    /// </summary>
+    /// <param name="owner"></param>
+    public override void Enter(Player owner)
+    {
+        owner.CurrentVelocity = Vector2.zero;
+        owner.AudioSource.Stop();
+        owner.Animator.SetFloat(Player.SpeedHash, 0f);
+    }
+
+    /// <summary>
+    /// Empty, since there are no input readings or state transitions during this state.
+    /// </summary>
+    /// <param name="owner">Unused.</param>
+    public override void Update(Player owner) { }
+
+    /// <summary>
+    /// Empty, since there are no input readings or state transitions during this state.
+    /// </summary>
+    /// <param name="owner">Unused</param>
+    public override void FixedUpdate(Player owner) { }
+
+    /// <summary>
+    /// Empty, since there are no state transitions from this state.
+    /// </summary>
+    /// <param name="owner">Unused.</param>
+    public override void Exit(Player owner) { }
+
+    /// <summary>
+    /// Empty, since we want to prevent the Player from taking damge whilst dead.
+    /// </summary>
+    /// <param name="owner">Unused.</param>
+    /// <param name="amount">Unused.</param>
+    public override void HandleDamage(Player owner, int amount) { }
 }
