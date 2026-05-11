b5b25bdfdbe647e0b23305acbdd2a1418e117b70
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-11 18:15:24 +0100

    feat: started working on the context of vending machine enemies
    - Made VendingMachineChargingState.cs extend VendingMachineState
    - Made VendingMachineMovingState.cs extend VendingMachineState
    - Made VendingMachineState extend State<VendingMachine>
    - Implemented basic context for VendingMachine enemies: hashes for moving animations, particle effects, states
    - Implemented 'OnProjectileHit' for VendingMachine.cs such that it destroys the object on collision with a projectile

--- Assets/_2DAdventureGame/Scripts/Entities/Enemy/Instances/Vending Machine/VendingMachine.cs
+++ Assets/_2DAdventureGame/Scripts/Entities/Enemy/Instances/Vending Machine/VendingMachine.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
 
 public class VendingMachine : Enemy
-{
-    protected override void OnProjectileHit()
+{  
+    public static readonly int MoveXHash = Animator.StringToHash("Move X");
+    public static readonly int MoveYHash = Animator.StringToHash("Move Y");
+
+    [Header("Vending Machine Assets")]
+    [SerializeField] private ParticleSystem smokeEffect;
+    public ParticleSystem SmokeEffect => smokeEffect;
+
+    public int Direction { get; set; }
+
+    public VendingMachineState CurrentState { get; private set; }
+    public VendingMachineMovingState MovingState { get; private set; }
+    public VendingMachineChargingState ChargingState { get; private set; }
+
+    protected override void Awake()
     {
-        throw new System.NotImplementedException();
+        base.Awake();
+
+        MovingState = new VendingMachineMovingState();
+        ChargingState = new VendingMachineChargingState();
+
+        CurrentState = MovingState;
+        CurrentState.Enter(this);
     }
+
+    void Update() => CurrentState.Update(this);
+
+    void FixedUpdate() => CurrentState.Update(this);
+    
+    public void ChangeState(VendingMachineState newState)
+    {
+        CurrentState.Exit(this);
+        CurrentState = newState;
+        CurrentState.Enter(this);
+    }
+
+    protected override void OnProjectileHit() => Destroy(gameObject);
 }
