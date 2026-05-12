ceb067952f88e2e00f147b2db80e0f309fc7f49c
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-12 10:59:56 +0100

    feat: implemented a basic charging motion for vending machine enemies

--- Assets/_2DAdventureGame/Scripts/Entities/Enemy/Instances/Vending Machine/States/Instances/VendingMachineChargingState.cs
+++ Assets/_2DAdventureGame/Scripts/Entities/Enemy/Instances/Vending Machine/States/Instances/VendingMachineChargingState.cs
@@ -3,4 +3,32 @@ using UnityEngine;
 public class VendingMachineChargingState : VendingMachineState
 {
     public override string ID => "Charging";
+
+    private float timer;
+    private Vector2 direction;
+
+    public override void Enter(VendingMachine owner)
+    {
+        timer = 0f;
+        direction = owner.Player.Rigidbody.position - owner.Rigidbody.position;
+    }
+
+    public override void Update(VendingMachine owner)
+    {
+        timer += Time.deltaTime;
+        if (timer >= owner.Data.chargeDuration)
+        {
+            owner.ChangeState(owner.MovingState);
+        }
+    }
+
+    public override void FixedUpdate(VendingMachine owner)
+    {
+        float progress = timer / owner.Data.chargeDuration;
+        float speed = owner.Data.speedCurve.Evaluate(progress) * owner.Data.maxSpeed;
+
+        owner.Rigidbody.MovePosition(
+            owner.Rigidbody.position + direction * (speed * Time.deltaTime)
+        );
+    }
 }
