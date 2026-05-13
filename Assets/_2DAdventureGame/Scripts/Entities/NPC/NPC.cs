b6722f89b97da6953f46c33a5f597a2c37afc4b8
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-13 16:36:10 +0100

    feat: implemented a simple mechanism to display NPC dialogue with lines explictly set in the Editor

--- Assets/_2DAdventureGame/Scripts/Entities/NPC/NPC.cs
+++ Assets/_2DAdventureGame/Scripts/Entities/NPC/NPC.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
     public GameObject dialogueBubble;
+    public List<string> dialogueLines = new List<string>();
+    private int lineIndex;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        dialogueBubble.SetActive(false);    
+        dialogueBubble.SetActive(false);
+        lineIndex = 0;
+    }
+
+    public void Talk()
+    {
+        UIHandler.Instance.DisplayDialogueWithLine(dialogueLines[lineIndex++]);
+        if (lineIndex >= dialogueLines.Count) lineIndex = 0;
     }
 }
