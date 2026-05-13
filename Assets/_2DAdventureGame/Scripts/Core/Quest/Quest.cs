c6fd84434ad2eadee5df0a0ddd845db3dc05bdf8
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-13 19:58:23 +0100

    feat: created a Quest scriptable object
    - Created a new scriptable object for quests called Quest.cs
    - Contains two fields: an id (string) and a description (string)

---
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/Quest.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Quest", menuName = "Game/Quest Data")]
+public class Quest : ScriptableObject
+{
+    public string id;
+    public string description;
+}
