2ab343f458961145662b6c8c9d664b43b83dd5f2
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-13 21:02:45 +0100

    feat: created a basic skeleton for the quest system

--- Assets/_2DAdventureGame/Scripts/Core/Quest/Objective/Instances/FetchObjective.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/Objective/Instances/FetchObjective.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "FetchObjective", menuName = "Scriptable Objects/FetchObjective")]
 public class FetchObjective : QuestObjective
 {
-    
+    public string item;
+    public int count;
+
+    public override bool Matches(QuestReport report) => report.type == ReportType.Fetch && report.tag == item;
 }
