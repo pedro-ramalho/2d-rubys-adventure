7d81a9499e35aa46d49672fa2c98318f0b2118dd
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-14 20:34:17 +0100

    feat: progress on quest system

--- Assets/_2DAdventureGame/Scripts/Core/Quest/Objectives/Instances/KillObjective.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/Objectives/Instances/KillObjective.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "KillObjective", menuName = "Scriptable Objects/KillObjective")]
 public class KillObjective : QuestObjective
 {
+    private int count;
+    private string enemy;
+    
     public override QuestObjectiveType Type => QuestObjectiveType.Kill;
+    public override int Count => count;
 
-    public string enemy;
-    public int count;
+    public override bool Matches(QuestReport report) => report.Type == Type && report.Tag == enemy;
 }
--- Assets/_2DAdventureGame/Scripts/Core/Quest/Objectives/QuestObjective.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/Objectives/QuestObjective.cs
@@ -6,4 +6,6 @@ public enum QuestObjectiveType { Fetch, Kill }
 public abstract class QuestObjective : ScriptableObject
 {
     public abstract QuestObjectiveType Type { get; }
+    public abstract int Count { get; }
+    public abstract bool Matches(QuestReport report);
 }
