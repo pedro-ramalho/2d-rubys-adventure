ab89e2742beb9e872cfd055bd533338c16f739a3
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-15 20:40:41 +0100

    feat: reworked OnTriggerEnter2D of Collectible.cs, simplified HealthCollectible.cs
    - In Collectible.cs, OnTriggerEnter2D will only call effect functionality if 'other' is of type Player
    - Wired the quest reporting to the base class, Collectible.cs
    - Simplified ApplyEffect of HealthCollectible.cs

--- Assets/_2DAdventureGame/Scripts/Environment/Collectibles/Collectible.cs
+++ Assets/_2DAdventureGame/Scripts/Environment/Collectibles/Collectible.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public abstract class Collectible : MonoBehaviour
 {
     [SerializeField] protected AudioClip collectibleClip;
-    public abstract void ApplyEffect(Collider2D other);
+    public abstract void ApplyEffect(Player player);
     protected virtual void OnEffectApplied() => Destroy(gameObject);
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        ApplyEffect(other);
-        
-        if (other.TryGetComponent(out Player _))
+        if (other.TryGetComponent(out Player player))
+        {
+            ApplyEffect(player);
             OnEffectApplied();
+            GetComponent<QuestReporter>()?.Report();
+        }
     }         
 }
