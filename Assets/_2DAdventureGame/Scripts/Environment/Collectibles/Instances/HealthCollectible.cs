94e3981e112e23e2b5f4a0381964a67451a54a42
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-15 20:40:41 +0100

    feat: reworked OnTriggerEnter2D of Collectible.cs, simplified HealthCollectible.cs
    - In Collectible.cs, OnTriggerEnter2D will only call effect functionality if 'other' is of type Player
    - Wired the quest reporting to the base class, Collectible.cs
    - Simplified ApplyEffect of HealthCollectible.cs

--- Assets/_2DAdventureGame/Scripts/Environment/Collectibles/Instances/HealthCollectible.cs
+++ Assets/_2DAdventureGame/Scripts/Environment/Collectibles/Instances/HealthCollectible.cs
@@ -5,17 +5,10 @@ public class HealthCollectible : Collectible
     [SerializeField] private string reportTag;
     [SerializeField] private int healAmount;
 
-    public override void ApplyEffect(Collider2D other)
+    public override void ApplyEffect(Player player)
     {
-        if (other.TryGetComponent(out Player player))
-        {
-            if (player.CurrentHealth < player.Data.maxHealth)
-                player.Heal(healAmount);
-            
-            player.GetComponent<AudioSource>()?.PlayOneShot(collectibleClip);
-
-            GetComponent<QuestReporter>()?.Report();
-        }
+        if (player.CurrentHealth < player.Data.maxHealth) player.Heal(healAmount);
+        
+        player.GetComponent<AudioSource>()?.PlayOneShot(collectibleClip);
     }
-
 }
