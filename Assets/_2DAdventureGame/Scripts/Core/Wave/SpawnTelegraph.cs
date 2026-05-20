29da31c943f46a20a8178f9ecc55cce52498bc12
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-20 20:59:05 +0100

    feat: created a script to handle spawning the telegraph effect

--- Assets/_2DAdventureGame/Scripts/Core/Wave/SpawnTelegraph.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Wave/SpawnTelegraph.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
-public class SpawnTelegraph
+public class SpawnTelegraph : MonoBehaviour
 {
-    
+    [SerializeField] private float lifetime = 0.6f;
+
+    void Start() => Destroy(gameObject, lifetime);
 }
