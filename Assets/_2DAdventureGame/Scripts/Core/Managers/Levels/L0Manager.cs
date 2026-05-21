b6814a3bd6bae67606330cd106f5b3f81dc4ad04
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-21 11:29:31 +0100

    feat: delete GameManager.cs, implement L0Manager.cs

--- Assets/_2DAdventureGame/Scripts/Core/Managers/Levels/L0Manager.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Managers/Levels/L0Manager.cs
@@ -1,16 +1,6 @@
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class L0Manager : MonoBehaviour
+public class L0Manager
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void LoadNextLevel() => SceneManager.LoadScene("Level 1");
 }
