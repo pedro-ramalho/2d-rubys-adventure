8df7a129afcf5c4f1cca9a4b0d03d5a4b312b95d
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-21 11:12:33 +0100

    feat: implemented the manager for level 2
    - Implemented the L2Manager class: subscribes to "OnDied" and "OnAllWavesCleared"
    - If the player dies, the game ends and the UI handler displays the losing screen
    - If the player wins by defeating all enemy waves, the game also ends and the UI handler displays the winning screen

--- Assets/_2DAdventureGame/Scripts/Core/Managers/Levels/L2Manager.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Managers/Levels/L2Manager.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class L2Manager : MonoBehaviour
 {
+    [SerializeField] private Player player;
+    [SerializeField] private WaveSpawner spawner;
+    [SerializeField] private UIHandler ui;
+    [SerializeField] private float endGameDelay = 3f;
+
+    private bool gameEnded = false;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player.OnDied += HandlePlayerDied;
+        spawner.OnAllWavesCleared += HandleAllWavesCleared;    
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        if (player != null)
+            player.OnDied -= HandlePlayerDied;
+
+        if (spawner != null)
+            spawner.OnAllWavesCleared -= HandleAllWavesCleared;
     }
+
+    void HandlePlayerDied() => EndGame(win: false);
+    void HandleAllWavesCleared() => EndGame(win: true);
+
+    void EndGame(bool win)
+    {
+        if (gameEnded) return;
+        gameEnded = true;
+
+        if (win) 
+            ui.DisplayWinScreen();
+        else
+            ui.DisplayLoseScreen();
+
+        Invoke(nameof(ReloadScene), endGameDelay);
+    }
+
+    void ReloadScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 }
