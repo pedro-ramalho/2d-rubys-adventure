42e29e6fe5b0922d9d1fd9d5a843d0ef55fc98f5
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-21 11:44:01 +0100

    feat: renamed 'OnHit' in IDamageable.cs to 'ApplyDamage'

--- Assets/_2DAdventureGame/Scripts/Environment/Damage Zones/DamageZone.cs
+++ Assets/_2DAdventureGame/Scripts/Environment/Damage Zones/DamageZone.cs
@@ -5,6 +5,6 @@ public class DamageZone : MonoBehaviour
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.TryGetComponent(out IDamageable damageable))
-            damageable.OnHit(amount: 1);
+            damageable.ApplyDamage(amount: 1);
     }
 }
