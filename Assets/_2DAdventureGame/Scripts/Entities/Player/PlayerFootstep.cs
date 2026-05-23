b8681cf1ea879d8b064e858ec89fb8083507efc3
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-23 13:01:07 +0100

    feat: implemented PlayerFootstep.cs

--- Assets/_2DAdventureGame/Scripts/Entities/Player/PlayerFootstep.cs
+++ Assets/_2DAdventureGame/Scripts/Entities/Player/PlayerFootstep.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerFootstep : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip[] clips;
 
-    // Update is called once per frame
-    void Update()
+    [Tooltip("Random pitch variation in either direction")]
+    [SerializeField, Range(0f, 0.3f)] private float pitchVariation = 0.05f;
+
+    public void PlayFootstep()
     {
+        if (source == null || clips == null || clips.Length == 0) return;
+
+        AudioClip clip = clips[Random.Range(0, clips.Length)];
         
+        source.pitch = 1f + Random.Range(-pitchVariation, pitchVariation);
+        source.PlayOneShot(clip);
     }
 }
