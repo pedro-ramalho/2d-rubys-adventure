517ef4038ef4e922a13635083b062fbb623f9150
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-27 11:24:14 +0100

    feat: add a Count property to Quest.cs

--- Assets/_2DAdventureGame/Scripts/Core/Quest/Quest.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/Quest.cs
@@ -2,18 +2,17 @@ using UnityEngine;
 
 public class Quest
 {
-    private int count;
-
     public QuestData Data { get; }
+    public int Count { get; private set; }
 
     public Quest(QuestData data) => Data = data;
 
-    public bool IsComplete => count >= Data.objective.Count;
+    public bool IsComplete => Count >= Data.objective.Count;
 
     public void ApplyProgress(QuestReport report)
     {
         if (IsComplete) return;
 
-        if (Data.objective.Matches(report)) count++;
+        if (Data.objective.Matches(report)) Count++;
     }
 }
