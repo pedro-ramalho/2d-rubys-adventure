ba171fa99d82f815cb5cab3457b1d1980c69fd74
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-05-28 10:21:28 +0100

    refactor: modified Patrol Robots ti play an effect when they are fixed

--- Assets/_2DAdventureGame/Scripts/Entities/Enemy/Instances/Patrol Robot/States/Instances/PatrolRobotFixedState.cs
+++ Assets/_2DAdventureGame/Scripts/Entities/Enemy/Instances/Patrol Robot/States/Instances/PatrolRobotFixedState.cs
@@ -10,6 +10,8 @@ public class PatrolRobotFixedState : PatrolRobotState
         owner.Animator.SetTrigger(PatrolRobot.FixedHash);
         owner.AudioSource.Stop();
         if (owner.FixedClip != null) owner.AudioSource.PlayOneShot(owner.FixedClip);
+        if (owner.FixedEffectPrefab != null)
+            Object.Instantiate(owner.FixedEffectPrefab, owner.transform.position, Quaternion.identity);
         owner.SmokeEffect.Stop();
         owner.RaiseOnFixed();
         owner.GetComponent<QuestReporter>()?.Report();
