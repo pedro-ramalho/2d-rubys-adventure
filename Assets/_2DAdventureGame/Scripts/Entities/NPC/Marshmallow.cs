c1cad74b25d23a3f951410b83593341744549bae
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-01 20:27:24 +0100

    feat: implement Marshmallow.cs, a controller script for the Marshmallow NPC in Level 2

---
+++ Assets/_2DAdventureGame/Scripts/Entities/NPC/Marshmallow.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Marshmallow : MonoBehaviour
+{
+    [SerializeField] private QuestData boundQuest;
+    [SerializeField] private Transform exitPoint;
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private Vector2 idleFacing = Vector2.down;
+
+    private static readonly int SpeedHash = Animator.StringToHash("Speed");
+    private static readonly int LookXHash = Animator.StringToHash("Look X");
+    private static readonly int LookYHash = Animator.StringToHash("Look Y");
+
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        SetFacing(idleFacing);
+    }
+
+    void OnEnable()
+    {
+        if (QuestManager.Instance != null)
+            QuestManager.Instance.OnQuestAccepted += HandleQuestAccepted;
+    }
+
+    void OnDisable()
+    {
+        if (QuestManager.Instance != null)
+            QuestManager.Instance.OnQuestAccepted -= HandleQuestAccepted;
+    }
+
+    void HandleQuestAccepted(Quest quest)
+    {
+        if (quest.Data == boundQuest) WalkOffscreen();
+    }
+
+    public void WalkOffscreen()
+    {
+        if (exitPoint != null) StartCoroutine(WalkTo(exitPoint.position));
+    }
+
+    IEnumerator WalkTo(Vector2 target)
+    {
+        SetCollidersEnabled(false);
+
+        Vector2 direction = (target - rb.position).normalized;
+        SetFacing(direction);
+        animator.SetFloat(SpeedHash, 1f);
+
+        WaitForFixedUpdate wait = new();
+        while (Vector2.Distance(rb.position, target) > 0.01f)
+        {
+            Vector2 next = Vector2.MoveTowards(rb.position, target, walkSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(next);
+            
+            yield return wait;
+        }
+
+        SetFacing(idleFacing);
+        animator.SetFloat(SpeedHash, 0f);
+
+        SetCollidersEnabled(true);
+    }
+
+    void SetFacing(Vector2 direction)
+    {
+        animator.SetFloat(LookXHash, direction.x);
+        animator.SetFloat(LookYHash, direction.y);
+    }
+
+    void SetCollidersEnabled(bool value)
+    {
+        foreach (Collider2D collider in GetComponents<Collider2D>())
+            collider.enabled = value;
+    }
+}
