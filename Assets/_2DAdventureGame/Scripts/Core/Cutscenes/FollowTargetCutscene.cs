f3359a6c26c5c0e993d57df7317e6d757e0808f1
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-01 21:32:28 +0100

    feat: implement FollowTargetCutscene.cs

--- Assets/_2DAdventureGame/Scripts/Core/Cutscenes/FollowTargetCutscene.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Cutscenes/FollowTargetCutscene.cs
@@ -1,6 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
-public class FollowTargetCutscene
+public class FollowTargetCutscene : Cutscene
 {
-    
+    [SerializeField] private Transform target;
+
+    private bool finished;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (target != null) cam.Follow = target;
+    }
+
+    public void Begin()
+    {
+        if (CutsceneManager.Active) return;
+        finished = false;
+        StartCoroutine(Play());
+    }
+
+    public void Finish() => finished = true;
+
+    protected override IEnumerator Run()
+    {
+        yield return new WaitUntil(() => finished);
+    }
 }
