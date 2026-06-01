5aea3062b53997d21c5c46ba14330868ed933e7b
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-01 21:30:07 +0100

    feat: implement Cutscene.cs

--- Assets/_2DAdventureGame/Scripts/Core/Cutscenes/Cutscene.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Cutscenes/Cutscene.cs
@@ -1,6 +1,35 @@
+using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
-public class Cutscene
+[RequireComponent(typeof(CinemachineCamera))]
+public abstract class Cutscene : MonoBehaviour
 {
-    
+    [SerializeField] protected int activePriority = 100;
+
+    protected CinemachineCamera cam;
+    private int dormantPriority;
+
+    protected virtual void Awake()
+    {
+        cam = GetComponent<CinemachineCamera>();
+        dormantPriority = cam.Priority;
+    }
+
+    public IEnumerator Play()
+    {
+        if (CutsceneManager.Instance != null)
+            CutsceneManager.Instance.IsActive = true;
+
+        cam.Priority = activePriority;
+
+        yield return Run();
+
+        cam.Priority = dormantPriority;
+
+        if (CutsceneManager.Instance != null)
+            CutsceneManager.Instance.IsActive = false;
+    }
+
+    protected abstract IEnumerator Run();
 }
