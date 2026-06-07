0f71c87307e2c550633637cdf22fbb0a02359c21
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-07 10:38:12 +0100

    feat: implement MainMenuHandler.cs

--- Assets/_2DAdventureGame/Scripts/UI/MainMenuHandler.cs
+++ Assets/_2DAdventureGame/Scripts/UI/MainMenuHandler.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
 public class MainMenuHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private string firstLevelSceneName = "Level 0";
+
     void Start()
     {
-        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button startButton = root.Q<Button>("StartButton");
+        Button continueButton = root.Q<Button>("ContinueButton");
+        Button optionsButton = root.Q<Button>("OptionsButton");
+        Button quitButton = root.Q<Button>("QuitButton");
+
+        startButton.clicked += StartGame;
+        quitButton.clicked += QuitGame;
+
+        continueButton.SetEnabled(false);
+        optionsButton.SetEnabled(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartGame() => SceneManager.LoadScene(firstLevelSceneName);
+
+    void QuitGame()
     {
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
