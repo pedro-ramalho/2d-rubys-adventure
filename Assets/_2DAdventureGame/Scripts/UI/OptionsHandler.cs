fc008772113531111f96d25d3d8deb8d08579f32
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-08 17:35:24 +0100

    feat: split volume control into Master/Music/SFX/Ambient mixer sliders
    - Each slider in the options menu drives an exposed AudioMixer parameter

--- Assets/_2DAdventureGame/Scripts/UI/OptionsHandler.cs
+++ Assets/_2DAdventureGame/Scripts/UI/OptionsHandler.cs
@@ -1,10 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(UIDocument))]
 public class OptionsHandler : MonoBehaviour
 {
+    [SerializeField] private AudioMixer audioMixer;
+
+    private const float DefaultVolume = 1f;
+    private const float MinDb = -80f;
+
     public event Action Opened;
     public event Action Closed;
 
@@ -14,8 +20,34 @@ public class OptionsHandler : MonoBehaviour
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         optionsRoot = root.Q<VisualElement>("OptionsRoot");
+
         Button backButton = root.Q<Button>("OptionsBackButton");
         backButton.clicked += Close;
+
+        WireVolumeSlider(root, "MasterVolumeSlider", "MasterVolume");
+        WireVolumeSlider(root, "MusicVolumeSlider", "MusicVolume");
+        WireVolumeSlider(root, "SfxVolumeSlider", "SfxVolume");
+        WireVolumeSlider(root, "AmbientVolumeSlider", "AmbientVolume");
+    }
+
+    void WireVolumeSlider(VisualElement root, string sliderName, string mixerParam)
+    {
+        Slider slider = root.Q<Slider>(sliderName);
+        float saved = PlayerPrefs.GetFloat(mixerParam, DefaultVolume);
+        slider.SetValueWithoutNotify(saved);
+        ApplyVolume(mixerParam, saved);
+
+        slider.RegisterValueChangedCallback(evt =>
+        {
+            ApplyVolume(mixerParam, evt.newValue);
+            PlayerPrefs.SetFloat(mixerParam, evt.newValue);
+        });
+    }
+
+    void ApplyVolume(string mixerParam, float linear)
+    {
+        float dB = linear > 0.0001f ? Mathf.Log10(linear) * 20f : MinDb;
+        audioMixer.SetFloat(mixerParam, dB);
     }
 
     public void Open()
