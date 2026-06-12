8a9d5f63a7c6c8fcdf40b84a35bcd18ab0ecc1d3
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-12 18:01:09 +0100

    feat: updated SceneMusicConfigManager.cs

--- Assets/_2DAdventureGame/Scripts/Core/Managers/SceneMusicConfigManager.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Managers/SceneMusicConfigManager.cs
@@ -18,7 +18,11 @@ public class SceneMusicConfigManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (MusicManager.Instance != null && defaultTrack != null)
+        if (MusicManager.Instance == null) return;
+
+        if (defaultTrack != null)
             MusicManager.Instance.Play(defaultTrack);
+        else
+            MusicManager.Instance.FadeOutAndStop(5f);
     }
 }
