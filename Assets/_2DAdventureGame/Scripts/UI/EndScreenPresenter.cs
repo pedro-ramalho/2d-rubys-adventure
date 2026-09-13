d11d082c98504a2f325e3a3e6c5efb8da2c5a2e1
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-09-13 17:41:24 +0100

    refactor: extracted the end screen logic from UIHandler to EndScreenPresenter.cs

---
+++ Assets/_2DAdventureGame/Scripts/UI/EndScreenPresenter.cs
@@ -0,0 +1,55 @@
+using AdventureGame.Core.Managers;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AdventureGame.UI
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class EndScreenPresenter : MonoBehaviour
+    {
+        public static EndScreenPresenter Instance { get; private set; }
+
+        [SerializeField]
+        private AudioSource m_StingerSource;
+
+        [SerializeField]
+        private AudioClip m_DefeatSting;
+
+        [SerializeField]
+        private float m_MusicFadeOnEndScreen = 2f;
+
+        private VisualElement m_WinScreen;
+        private VisualElement m_LoseScreen;
+
+        void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+        }
+
+        void Start()
+        {
+            VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+            
+            m_WinScreen = root.Q<VisualElement>("WinScreenContainer");
+            m_LoseScreen = root.Q<VisualElement>("LoseScreenContainer");
+        }
+
+        public void DisplayWinScreen()
+        {
+            if (m_WinScreen != null)
+                m_WinScreen.style.opacity = 1.0f;
+        }
+
+        public void DisplayLoseScreen()
+        {
+            if (m_LoseScreen != null)
+                m_LoseScreen.style.opacity = 1.0f;
+
+            MusicManager.Instance?.FadeOutAndStop(m_MusicFadeOnEndScreen);
+
+            if (m_StingerSource != null && m_DefeatSting != null)
+                m_StingerSource.PlayOneShot(m_DefeatSting);
+        }
+    }
+}
