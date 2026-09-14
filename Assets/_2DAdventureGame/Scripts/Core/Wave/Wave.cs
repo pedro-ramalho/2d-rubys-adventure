734045e6468af3c7c08dc0030111bab66697756f
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-09-14 16:34:32 +0100

    refactor: split Marshmallow.cs into a choreographer, fixed issue with Player footsteps

--- Assets/_2DAdventureGame/Scripts/Core/Wave/Wave.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Wave/Wave.cs
@@ -1,12 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace AdventureGame.Core.Wave
 {
     [Serializable]
     public class Wave
     {
+        [FormerlySerializedAs("enemyPrefab")]
         public GameObject EnemyPrefab;
+
+        [FormerlySerializedAs("enemyCount")]
         public int EnemyCount;
     }
 }
