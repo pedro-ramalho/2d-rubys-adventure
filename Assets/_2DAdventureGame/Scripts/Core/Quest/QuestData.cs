33cccccbe51345cf7e1af6ffee786be465e75d77
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-09-14 12:26:21 +0100

    refactor: extract ability unlocks out of QuestController.cs

--- Assets/_2DAdventureGame/Scripts/Core/Quest/QuestData.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/QuestData.cs
@@ -1,18 +1,25 @@
 using AdventureGame.Core.Managers;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace AdventureGame.Core.Quest
 {
     [CreateAssetMenu(fileName = "QuestData", menuName = "Game/Quest")]
     public class QuestData : ScriptableObject
     {
+        [FormerlySerializedAs("id")]
         public string Id;
+
+        [FormerlySerializedAs("description")]
         public string Description;
 
+        [FormerlySerializedAs("targetCount")]
         public int TargetCount;
 
+        [FormerlySerializedAs("unlockOnAccept")]
         public AbilityFlag UnlockOnAccept = AbilityFlag.None;
 
+        [FormerlySerializedAs("backgroundTrack")]
         public AudioClip BackgroundTrack;
     }
 }
--- Assets/_2DAdventureGame/Scripts/Core/Quest/QuestDialogue.cs
+++ Assets/_2DAdventureGame/Scripts/Core/Quest/QuestDialogue.cs
@@ -1,17 +1,22 @@
 using AdventureGame.Core.Dialogue;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace AdventureGame.Core.Quest
 {
     [CreateAssetMenu(fileName = "QuestDialogue", menuName = "Game/Quest Dialogue")]
     public class QuestDialogue : ScriptableObject
     {
+        [FormerlySerializedAs("quest")]
         public QuestData Quest;
 
+        [FormerlySerializedAs("before")]
         public DialoguePhase Before;
 
+        [FormerlySerializedAs("during")]
         public DialoguePhase During;
 
+        [FormerlySerializedAs("after")]
         public DialoguePhase After;
 
         public DialoguePhase Pick(QuestController controller)
